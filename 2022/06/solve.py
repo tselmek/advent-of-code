3c046b6eeef1ba111fd8fbec8dcc152cca04e643
Antoine Jésus <antoine.jesus@iwecloud.com>
2022-12-06 08:59:22 +0100

    2022 Day 6

---
+++ 2022/06/solve.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple
+
+
+lines = [line.rstrip("\n") for line in open("input.txt")]
+
+message = lines[0]
+
+def get_marker_index(packet: str) -> int:
+  k = 0
+  SIZE = 4 if packet == "packet" else 14
+
+  while len(set(message[k:k+SIZE])) < SIZE:
+    k += 1
+
+  return k + SIZE
+
+print(get_marker_index("packet"))
+
+print(get_marker_index("message"))
